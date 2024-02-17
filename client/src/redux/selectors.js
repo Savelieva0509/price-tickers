31d582d65473f07d683b66bb13399430a7ebf223
Babenko Liudmyla <savelieva0509@gmail.com>
2024-02-17 17:24:10 +0200

    add selectors

---
+++ client/src/redux/selectors.js
@@ -0,0 +1,3 @@
+export const selectTickers = (state) => state.tickers.tickers;
+export const selectStatus = (state) => state.tickers.status;
+export const selectError = (state) => state.tickers.error;
