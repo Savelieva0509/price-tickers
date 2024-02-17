bf4bb4c20b5d64726dad5644df9e03de3a544286
Babenko Liudmyla <savelieva0509@gmail.com>
2024-02-17 17:24:41 +0200

    Update tickersSlice.js

--- client/src/redux/tickersSlice.js
+++ client/src/redux/tickersSlice.js
@@ -4,16 +4,22 @@ export const tickerSlice = createSlice({
   name: "tickers",
   initialState: {
     tickers: [],
+    status: "idle",
+    error: null,
   },
   reducers: {
     updateTickers: (state, action) => {
       state.tickers = action.payload;
+      },
+    updateStatus: (state, action) => {
+      state.status = action.payload;
+    },
+    updateError: (state, action) => {
+      state.error = action.payload;
     },
   },
 });
 
-export const { updateTickers } = tickerSlice.actions;
-
-export const selectTickers = (state) => state.tickers.tickers;
+export const { updateTickers, updateError, updateStatus } = tickerSlice.actions;
 
 export default tickerSlice.reducer;
