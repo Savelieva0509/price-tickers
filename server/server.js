fc1c71b4d5a8c8ac3708e8e417aa86c73bde85c7
Babenko Liudmyla <savelieva0509@gmail.com>
2024-02-18 17:19:17 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
-'use strict';
-const express = require('express');
-const http = require('http');
-const io = require('socket.io');
-const cors = require('cors');
+"use strict";
+const express = require("express");
+const http = require("http");
+const io = require("socket.io");
+const cors = require("cors");
 
 const FETCH_INTERVAL = 5000;
 const PORT = process.env.PORT || 4000;
 
 const tickers = [
-  'AAPL', // Apple
-  'GOOGL', // Alphabet
-  'MSFT', // Microsoft
-  'AMZN', // Amazon
-  'FB', // Facebook
-  'TSLA', // Tesla
+  "AAPL", // Apple
+  "GOOGL", // Alphabet
+  "MSFT", // Microsoft
+  "AMZN", // Amazon
+  "FB", // Facebook
+  "TSLA", // Tesla
 ];
+
+const connectedClients = {};
 
 function randomValue(min = 0, max = 1, precision = 0) {
   const random = Math.random() * (max - min) + min;
@@ -23,12 +25,18 @@ function randomValue(min = 0, max = 1, precision = 0) {
 
 function utcDate() {
   const now = new Date();
-  return new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+  return new Date(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds()
+  );
 }
 
-function getQuotes(socket) {
-
-  const quotes = tickers.map((ticker) => ({
+function getQuotes(socket, subscribedTickers) {
+  const quotes = subscribedTickers.map((ticker) => ({
     ticker,
     exchange: "NASDAQ",
     price: randomValue(100, 300, 2),
@@ -39,19 +47,26 @@ function getQuotes(socket) {
     last_trade_time: utcDate(),
   }));
 
-  socket.emit('ticker', quotes);
+  socket.emit("ticker", quotes);
 }
 
 function trackTickers(socket) {
+  const clientId = socket.id;
+  const client = connectedClients[clientId];
+
+  if (!client) {
+    return;
+  }
+
   // run the first time immediately
-  getQuotes(socket);
+  getQuotes(socket, client.tickers);
 
   // every N seconds
-  const timer = setInterval(function() {
-    getQuotes(socket);
+  const timer = setInterval(function () {
+    getQuotes(socket, client.tickers);
   }, FETCH_INTERVAL);
 
-  socket.on('disconnect', function() {
+  socket.on("disconnect", function () {
     clearInterval(timer);
   });
 }
@@ -63,16 +78,37 @@ const server = http.createServer(app);
 const socketServer = io(server, {
   cors: {
     origin: "*",
-  }
+  },
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-socketServer.on('connection', (socket) => {
-  socket.on('start', () => {
+socketServer.on("connection", (socket) => {
+  socket.on("start", () => {
+    connectedClients[socket.id] = {
+      socket: socket,
+      tickers: tickers.slice(), // Копирование массива тикеров
+    };
     trackTickers(socket);
+  });
+
+  socket.on("delete", (ticker) => {
+    console.log(`Received delete request for ticker: ${ticker}`);
+    if (connectedClients[socket.id]) {
+      const index = connectedClients[socket.id].tickers.indexOf(ticker);
+      if (index !== -1) {
+        connectedClients[socket.id].tickers.splice(index, 1);
+        // Отправляем сообщение всем клиентам, чтобы они прекратили обновление тикера
+        socketServer.emit("tickerDeleted", ticker);
+        console.log(`Ticker ${ticker} deleted`);
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
+    delete connectedClients[socket.id];
   });
 });
 
