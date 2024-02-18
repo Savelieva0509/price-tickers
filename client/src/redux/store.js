5d1d924c5aaaa2776f24be1e84aaf37174601749
Babenko Liudmyla <savelieva0509@gmail.com>
2024-02-18 21:48:48 +0200

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,17 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import tickersReducer from './tickersSlice';
+import thunk from "redux-thunk";
+import tickersReducer from "./tickersSlice";
 import socketMiddleware from "./socketMiddleware";
+
+// Включаем redux-thunk в middleware только для тестов
+const middleware =
+  process.env.NODE_ENV === "test"
+    ? [...getDefaultMiddleware(), thunk, socketMiddleware()]
+    : [...getDefaultMiddleware(), socketMiddleware()];
 
 export const store = configureStore({
   reducer: {
     tickers: tickersReducer,
   },
-  middleware: [...getDefaultMiddleware(), socketMiddleware()],
+  middleware,
 });
