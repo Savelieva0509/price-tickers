56e17d746fe02b36d0d6bd86aa4c197bd22f00c1
Babenko Liudmyla <savelieva0509@gmail.com>
2024-02-18 17:19:22 +0200

    Update socket.js

--- client/src/helpers/socket.js
+++ client/src/helpers/socket.js
@@ -2,3 +2,8 @@ import io from 'socket.io-client';
 const socket = io("http://localhost:4000");
 socket.emit("start");
 export default socket;
+
+socket.deleteTicker = function (ticker) {
+  this.emit("delete", ticker);
+};
+
