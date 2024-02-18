998ffe3dd21249fc964866e0fc2fcb9ec0f3560c
Babenko Liudmyla <savelieva0509@gmail.com>
2024-02-18 17:19:51 +0200

    add del tickers

--- client/src/components/PriceTicker.js
+++ client/src/components/PriceTicker.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
 import fetchTickers from "../redux/operations";
 import { selectTickers, selectStatus, selectError } from "../redux/selectors";
+import socket from "../helpers/socket";
 
 const PriceTicker = () => {
   const dispatch = useDispatch();
   const tickers = useSelector(selectTickers);
   const status = useSelector(selectStatus);
   const error = useSelector(selectError);
+
+  const [isDeleting, setIsDeleting] = useState({});
 
   useEffect(() => {
     dispatch(fetchTickers());
@@ -23,20 +26,34 @@ const PriceTicker = () => {
     return (
       <div>
         <p>Error: {error}</p>
-            {error === "Disconnected from the server." && (
-                
-          <p class="alert alert-warning" role="alert">The server is currently offline. Please try again later.</p>
+        {error === "Disconnected from the server." && (
+          <p className="alert alert-warning" role="alert">
+            The server is currently offline. Please try again later.
+          </p>
         )}
       </div>
     );
   }
 
+  const handleDeleteTicker = (ticker) => {
+    // Обновляем состояние для отображения процесса удаления для конкретного тикера
+    setIsDeleting((prevIsDeleting) => ({
+      ...prevIsDeleting,
+      [ticker.ticker]: true,
+    }));
+
+    // Отправляем запрос на сервер для удаления тикера из списка обновляемых
+    socket.deleteTicker(ticker.ticker);
+  };
+
   return (
     <div className="container">
       <h2>Price Tickers</h2>
-      <table class="table table-bordered border-dark">
-        <thead class="table-dark">
+      <table className="table table-bordered border-dark">
+        <thead className="table-dark">
           <tr>
+            <th><button type="button" class="btn btn-light"> Show all
+                </button></th>
             <th>Ticker</th>
             <th>Price</th>
             <th>Change Percent</th>
@@ -46,6 +63,18 @@ const PriceTicker = () => {
         <tbody>
           {tickers.map((ticker) => (
             <tr key={ticker.ticker}>
+              <td>
+                <button
+                  type="button"
+                  className={`btn ${
+                    isDeleting[ticker.ticker] ? "btn-secondary" : "btn-dark"
+                  }`}
+                  onClick={() => handleDeleteTicker(ticker)}
+                  disabled={isDeleting[ticker.ticker]}
+                >
+                  {isDeleting[ticker.ticker] ? "Deleting..." : "Delete"}
+                </button>
+              </td>
               <td>{ticker.ticker}</td>
               <td>${ticker.price}</td>
               <td>
